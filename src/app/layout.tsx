92549dde372a7a3ee6d747cac86be1b0168d0f8f
Mariano Guillaume <marianguillaume.m@gmail.com>
2025-03-04 02:22:08 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Next.js with Tailwind CSS Starter',
+  description: 'Created by Mariano Guillaume (@mariandotg)',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang='en'>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
